2ed4d56be34ab9280d489acffb9eabc6a48812e1
Sam-Monique <moniques@msu.edu>
2023-05-18 12:08:00 -0400

    made a few small changes to viola_secar, added test environment for cycle counting

---
+++ pluginss/environments/cycle_count/__init__.py
@@ -0,0 +1,55 @@
+import numpy as np
+from badger import environment
+from badger.interface import Interface
+class Environment(environment.Environment):
+
+    name = 'cycle_count'
+
+    vranges = {
+        'x1': [-2,2],
+        'x2':[-2,2]
+    }
+
+    def __init__(self, interface: Interface, params):
+        super().__init__(interface, params)
+        self.variables = {
+            'x1': 2,
+            'x2':2
+        }
+        self.cycle = 0
+        
+
+    @staticmethod
+    def list_vars():
+        return ['x1','x2']
+    
+    @staticmethod
+    def list_obses():
+        return ['y1']
+    
+    @staticmethod
+    def get_default_params():
+        return None
+
+    def _get_vrange(self, var):
+        return self.vranges[var]
+
+    def _get_var(self, var):
+        return self.variables[var]
+
+    def _set_var(self, var, x):
+
+        if x > self.variables[var]:
+            self.cycle += 1
+        print(f"The Magnet has been cycled {self.cycle} times")
+        self.variables[var] = x
+    
+    def _get_obs(self, obs):
+        x1 = self.variables['x1']
+        x2 = self.variables['x2']
+
+
+        if obs == 'y1': 
+            return x1**2 + (x2-1)**2
+           
+
